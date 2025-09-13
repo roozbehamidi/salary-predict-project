5dfbfabc1bbc181a7767411e60c9a0f84217ca76
roozbehamidi <106980595+roozbehamidi@users.noreply.github.com>
2025-09-13 20:15:36 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,36 +21,14 @@ def encode_edlevel(value, columns):
 
 columns=["YearsCodePro"]
 
-country_list =['United Kingdom of Great Britain and Northern Ireland' 'Israel'
- 'Netherlands' 'United States of America' 'Czech Republic' 'Austria'
- 'Italy' 'Canada' 'Germany' 'Ireland' 'Poland' 'Madagascar' 'Norway'
- 'Taiwan' 'France' 'Brazil' 'Uruguay' 'Sweden' 'Spain' 'Turkey' 'Romania'
- 'Singapore' 'India' 'Belgium' 'Bulgaria' 'Greece' 'Portugal'
- 'Russian Federation' 'Saudi Arabia' 'Mexico' 'Kenya' 'Switzerland'
- 'Latvia' 'South Africa' 'Thailand' 'China' 'Montenegro' 'Finland'
- 'Slovakia' 'Japan' 'Denmark' 'Australia' 'Viet Nam' 'Argentina' 'Hungary'
- 'Tunisia' 'Bangladesh' 'Ukraine' 'Maldives' 'Hong Kong (S.A.R.)' 'Egypt'
- 'Serbia' 'Pakistan' 'Nepal' 'Croatia' 'Indonesia'
- 'Bosnia and Herzegovina' 'Armenia' 'Lithuania'
- 'Iran, Islamic Republic of...' 'Belarus' 'Costa Rica' 'Mauritius'
- 'Estonia' 'Kazakhstan' 'Morocco' 'Philippines' 'Chile' 'New Zealand'
- 'Slovenia' 'Ecuador' 'Cyprus' 'Peru' 'Colombia' 'Afghanistan' 'Nicaragua'
- 'Andorra' 'Republic of Korea' 'Lebanon' 'South Korea' 'Malaysia'
- 'Sri Lanka' 'Guatemala' 'Jordan' 'Azerbaijan' 'United Arab Emirates'
- 'Qatar' 'Nigeria' 'Uzbekistan' 'Ethiopia' 'Luxembourg'
- 'The former Yugoslav Republic of Macedonia' 'Syrian Arab Republic'
- 'Cambodia' 'Fiji' 'Albania' 'Mongolia' 'Republic of Moldova' 'Tajikistan'
- 'Timor-Leste' 'Ghana' 'United Republic of Tanzania' 'Algeria' 'Cameroon'
- 'Kosovo' 'Georgia' 'Turkmenistan' 'Botswana' 'Myanmar' 'Palestine'
- 'Senegal' 'Dominican Republic' 'Rwanda' 'Malta'
- 'Venezuela, Bolivarian Republic of...' 'Cuba' 'Kuwait' 'El Salvador'
- 'Bolivia' 'Isle of Man' 'Honduras' 'Mali' 'Panama'
- "Lao People's Democratic Republic" 'Iceland' 'Bahrain' 'Paraguay'
- 'Uganda' 'Democratic Republic of the Congo' 'Benin' "Côte d'Ivoire"
- 'Barbados' 'Kyrgyzstan' 'Nomadic' 'Iraq' 'Congo, Republic of the...'
- 'Libyan Arab Jamahiriya' 'Mozambique' 'Angola' 'Oman' 'Yemen' 'Sudan'
- 'Zambia' 'Somalia' 'Guinea' 'Zimbabwe' 'Cape Verde' 'Trinidad and Tobago'
- 'Bhutan' 'Togo' 'Suriname' 'Jamaica' 'Haiti' 'Malawi' 'Guyana' 'Palau'
- 'Monaco' 'Saint Lucia' 'Seychelles']
+country_list =['United Kingdom of Great Britain and Northern Ireland', 'Israel',
+       'Netherlands', 'United States of America', 'Czech Republic',
+       'Austria', 'Italy', 'Canada', 'Germany', 'Poland', 'Norway',
+       'France', 'Brazil', 'Sweden', 'Spain', 'Turkey', 'India',
+       'Belgium', 'Mexico', 'Switzerland', 'South Africa', 'Finland',
+       'Denmark', 'Australia', 'Greece', 'Portugal',
+       'Iran, Islamic Republic of...', 'Russian Federation', 'Pakistan',
+       'New Zealand']
+
 
 edlevel_list =['Master’s degree', 'Bachelor’s degree', 'Less than a Bachelors','Post grad']
